7f8af1a58f7bc5fe0de16f89a523a2f144b68971
Abhishek John Philip <abhishekjohnphilip@gmail.com>
2024-08-28 03:15:09 +0530

    commit

--- script.js
+++ script.js
@@ -36,18 +36,77 @@ gsap.to(".window-3 .closed", {
     }
 });
 
+gsap.to(".vector-clouds", {
+    x: 300, 
+    y: 0,  
+    scrollTrigger: {
+        trigger: ".window-line",
+        start: "top center",
+        end: "bottom center",
+        scrub: true,
+    }
+});
+
+gsap.to(".vector-birds", {
+    x: 200, 
+    y: 0, 
+    scrollTrigger: {
+        trigger: ".window-line",
+        start: "top center",
+        end: "bottom center",
+        scrub: true,
+    }
+});
+const circles = gsap.utils.toArray('.circle-container svg');
+
+circles.forEach((circle, index) => {
+    gsap.to(circle, {
+        rotation: 360,
+        scrollTrigger: {
+            trigger: "#intro-windows",
+            start: "top top",
+            end: "bottom top",
+            scrub: 1,
+        },
+        ease: "none",
+        duration: index + 1 
+    });
+});
+
 window.addEventListener("scroll", () => {
     let scrollPos = window.scrollY;
+    let windowHeight = window.innerHeight;
 
     gsap.to(".text-1", {
-        opacity: scrollPos > 100 ? 0 : 1,
+        opacity: scrollPos < windowHeight / 3 ? 1 : 0,
         duration: 1,
         ease: "power1.out"
     });
 
     gsap.to(".text-2", {
-        opacity: scrollPos > 100 ? 1 : 0,
+        opacity: scrollPos >= windowHeight / 3 && scrollPos < (2 * windowHeight) / 3 ? 1 : 0,
         duration: 1,
         ease: "power1.out"
     });
+
+    gsap.to(".text-3", {
+        opacity: scrollPos >= (2 * windowHeight) / 3 ? 1 : 0,
+        duration: 1,
+        ease: "power1.out"
+    });
+});
+
+
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    const introWindowsSection = document.querySelector('.intro-windows');
+    
+    // Calculate the bottom position of the intro-windows section
+    const introWindowsBottom = introWindowsSection.offsetTop + introWindowsSection.offsetHeight;
+    
+    if (window.scrollY >= introWindowsBottom) {
+        navbar.classList.add('show');
+    } else {
+        navbar.classList.remove('show');
+    }
 });
